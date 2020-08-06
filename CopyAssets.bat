93934450938b57bf4ed6ee05d309e018323d90d1
Robert Oze <oze.robi@gmail.com>
2020-08-06 09:38:08 +0200

    add engine assets and modify copy cmd

--- CopyAssets.bat
+++ CopyAssets.bat
@@ -1,10 +1,10 @@
-rem clear and copy Assets folder to Windows
+rem clear and copy AssetsMain folder to Windows
 del /s /q Solution\WindowsAndroid\Windows\Bin\Debug\x64\Assets\*
 del /s /q Solution\WindowsAndroid\Windows\Bin\Release\x64\Assets\*
 
-xcopy /s Assets Solution\WindowsAndroid\Windows\Bin\Debug\x64\Assets /e
-xcopy /s Assets Solution\WindowsAndroid\Windows\Bin\Release\x64\Assets /e
+xcopy /s /i AssetsMain Solution\WindowsAndroid\Windows\Bin\Debug\x64\Assets /e
+xcopy /s /i AssetsMain Solution\WindowsAndroid\Windows\Bin\Release\x64\Assets /e
 
-rem clear and copy Assets folder to Android
+rem clear and copy AssetsMain folder to Android
 del /s /q Solution\WindowsAndroid\Android\V3DEngineTests\V3DEngineTests.Packaging\assets\*
-xcopy /s Assets Solution\WindowsAndroid\Android\V3DEngineTests\V3DEngineTests.Packaging\assets /e
+xcopy /s /i AssetsMain Solution\WindowsAndroid\Android\V3DEngineTests\V3DEngineTests.Packaging\assets /e
