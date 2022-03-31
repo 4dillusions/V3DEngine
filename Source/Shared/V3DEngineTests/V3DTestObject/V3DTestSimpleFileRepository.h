67cc404a0695e2e6206fd22d6379b871a3802c5b
Robert Oze <support@4dillusions.com>
2022-03-31 09:20:33 +0200

    [V3DEngineTests] zemplatize V3DTestSimpleFileRepository

--- Source/Shared/V3DEngineTests/V3DTestObject/V3DTestSimpleFileRepository.h
+++ Source/Shared/V3DEngineTests/V3DTestObject/V3DTestSimpleFileRepository.h
@@ -6,16 +6,42 @@ Released under the terms of the GNU General Public License version 3 or later.
 
 #pragma once
 
+#include "V3DTestSimpleData.h"
 #include "V3DEngine/V3DData/V3DIFileRepository.h"
+#include "V3DEngine/V3DIO/V3DBinaryRW.h"
+#include "V3DEngine/V3DCore/V3DMemory.h"
 
 namespace V3D::V3DEngineTests::V3DTestObject
 {
-    struct V3DTestSimpleData;
-
-    class V3DTestSimpleFileRepository final : public V3D::V3DEngine::V3DData::V3DIFileRepository<V3DTestSimpleData>
+    template<typename TData> class V3DTestSimpleFileRepository final : public V3D::V3DEngine::V3DData::V3DIFileRepository<TData>
     {
     public:
-        V3DTestSimpleData Load(V3D::V3DEngine::V3DCore::V3DAssetPathType path, const char* fileName) const override;
-        void Save(V3DTestSimpleData& data, const char* fileName) const override;
+        TData Load(V3D::V3DEngine::V3DCore::V3DAssetPathType path, const char* fileName) const override
+        {
+            auto data = reinterpret_cast<TData*>(V3D::V3DEngine::V3DIO::V3DBinaryRW::Read(path, fileName));
+            const TData result = *data;
+            V3D::V3DEngine::V3DCore::V3DMemory::DeleteArray(data);
+
+            return result;
+        }
+        
+        void Save(TData& data, const char* fileName) const override
+        {
+	        V3D::V3DEngine::V3DIO::V3DBinaryRW::Write(fileName, reinterpret_cast<char*>(&data), sizeof(TData));
+        }
+    };
+
+    template<typename TData> class V3DTestSimpleFileRepository<TData*> final : public V3D::V3DEngine::V3DData::V3DIFileRepository<TData*>
+    {
+    public:
+        TData* Load(V3D::V3DEngine::V3DCore::V3DAssetPathType path, const char* fileName) const override
+        {
+            return reinterpret_cast<TData*>(V3D::V3DEngine::V3DIO::V3DBinaryRW::Read(path, fileName));
+        }
+
+        void Save(TData*& data, const char* fileName) const override
+        {
+            V3D::V3DEngine::V3DIO::V3DBinaryRW::Write(fileName, reinterpret_cast<char*>(data), sizeof(TData));
+        }
     };
 }
