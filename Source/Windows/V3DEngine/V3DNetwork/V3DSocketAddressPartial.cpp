57488d4813322016bdae7ef4a20660810c54a3e2
Robert Oze <support@4dillusions.com>
2022-04-10 17:20:50 +0200

    [V3DNetwork] add exception handling for V3DSocketAddress

--- Source/Windows/V3DEngine/V3DNetwork/V3DSocketAddressPartial.cpp
+++ Source/Windows/V3DEngine/V3DNetwork/V3DSocketAddressPartial.cpp
@@ -6,8 +6,12 @@ Released under the terms of the GNU General Public License version 3 or later.
 
 #include "V3DEngine/V3DNetwork/V3DSocketAddress.h"
 #include "V3DEngine/V3DNetwork/V3DNetworkSystem.h"
+#include "V3DEngine/V3DIO/V3DLogger.h"
 
 using namespace V3D::V3DEngine::V3DCore;
+using namespace V3D::V3DEngine::V3DIO;
+
+#include <cassert>
 
 namespace V3D::V3DEngine::V3DNetwork
 {
@@ -16,6 +20,34 @@ namespace V3D::V3DEngine::V3DNetwork
 		char host[256];
 		gethostname(host, sizeof host);
 		const hostent* host_entry = gethostbyname(host);  // NOLINT(concurrency-mt-unsafe)
+
+		if (host_entry == nullptr)
+		{
+			assert(false, "host_entry == nullptr");
+			V3DLogger::Get().WriteOutput(V3DLogMessageType::Error, "V3DNetwork::V3DSocketAddressGetLocalIp() - host_entry == nullptr");
+			return V3DCore::V3DString();
+		}
+
+		auto dwError = WSAGetLastError();
+		if (dwError == WSAHOST_NOT_FOUND)
+		{
+			assert(false, "Host not found");
+			V3DLogger::Get().WriteOutput(V3DLogMessageType::Error, "V3DNetwork::V3DSocketAddressGetLocalIp() - Host not found");
+			return V3DCore::V3DString();
+		}
+		if (dwError == WSANO_DATA)
+		{
+			assert(false, "No data record found");
+			V3DLogger::Get().WriteOutput(V3DLogMessageType::Error, "V3DNetwork::V3DSocketAddressGetLocalIp() - No data record found");
+			return V3DCore::V3DString();
+		}
+		if (dwError != 0)
+		{
+			assert(false, "Function failed with error");
+			V3DLogger::Get().WriteOutput(V3DLogMessageType::Error, "V3DNetwork::V3DSocketAddressGetLocalIp() - Function failed with error");
+			return V3DCore::V3DString();
+		}
+
 		char* result = inet_ntoa(*reinterpret_cast<struct in_addr*>(host_entry->h_addr_list[1]));  // NOLINT(concurrency-mt-unsafe)
 
 		return V3DString(result);
