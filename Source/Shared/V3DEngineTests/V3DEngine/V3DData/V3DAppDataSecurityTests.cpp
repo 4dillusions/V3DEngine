d666137b5a0c64d07dd3eaafaf9d8726541f3cdb
Robert Oze <support@4dillusions.com>
2022-08-10 18:39:09 +0200

    add new test sound assets

--- Source/Shared/V3DEngineTests/V3DEngine/V3DData/V3DAppDataSecurityTests.cpp
+++ Source/Shared/V3DEngineTests/V3DEngine/V3DData/V3DAppDataSecurityTests.cpp
@@ -17,7 +17,7 @@ namespace V3D::V3DEngineTests::V3DEngine::V3DData
 {
 	void V3DAppDataSecurityTests::AllAppDataValidTest()
 	{
-		V3DTest::AssertOk(V3DAppDataSecurity::GetIsAllAppDataValid(V3DString("secret key 123"), V3DString("eb441faf1564f5815599c28be20f23cfe8bf2c2640cbfaa8f8cf4fff2eec668c")), V3DFILE_INFO);
+		V3DTest::AssertOk(V3DAppDataSecurity::GetIsAllAppDataValid(V3DString("secret key 123"), V3DString("40a7c1f8450947471163ef4ff3d5e22f41e6f1590e435c55dc3979ebadb4b441")), V3DFILE_INFO);
 	}
 
 	void V3DAppDataSecurityTests::RunAllTests()
